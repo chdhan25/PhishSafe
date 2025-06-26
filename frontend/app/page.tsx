a1680b0eaa3903210bcae812b467ccd87670b8ae
chdhan25 <chdhan25@gmail.com>
2025-06-26 16:23:51 -0400

    re-add frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen justify-center items-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is working!
+      </h1>
+    </main>
+  );
+}
